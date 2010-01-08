3464d62ac871baeab2e1c8e412c481a6d57fee23
Matt Pruitt <guitsaru@gmail.com>
2010-01-08 11:16:06 -0600

    target_type in migration.

--- generators/timeline_fu/templates/migration.rb
+++ generators/timeline_fu/templates/migration.rb
@@ -8,6 +8,7 @@ class CreateTimelineEvents < ActiveRecord::Migration
 
     create_table :user_timeline_events do |t|
       t.integer  :target_id, :timeline_event_id
+      t.string   :target_type
       t.timestamps
     end
   end
